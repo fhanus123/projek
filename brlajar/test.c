a4d5229dfb1ff066cb38bf49927e45167f0a5e9c
fhanus <126766017+fhanus123@users.noreply.github.com>
2024-09-14 06:06:00 +0000

    runtel

--- brlajar/test.c
+++ brlajar/test.c
@@ -1,25 +1,41 @@
+// #include<stdio.h>
+// int main() {
+//     int true = 1;
+//     int false = 0;
+//     printf(" ini adalah nilai true dan true : %d \n", true && true);
+//     printf(" ini adalah nilai tre dan false : %d \n", true || false);
+//     printf(" ini adalah nilai false dan true : %d \n", false && true);
+//     printf(" ini adalah nilai false dan false : %d \n", false || false);
+//     printf(" ini adalah nilai not true : %d \n", !true);
+//     printf(" ini adalah nilai not false : %d \n", !false);
+//     printf("-----------\n");
+//     printf("OR T F \n");
+//     printf("T %d %d \n", true || true, true || false);
+//     printf("T %d %d \n",false || true, false || false);
+//     printf("-----------\n");
+
+//     printf("-----------\n");
+//     printf("AND %d %d \n",true && true,false || false);
+//     printf("%d %d %d \n",true || false,true || false,false && false);
+//     printf("%d %d %d \n",false || false, false && true,false && true);
+//     printf("-----------\n");
+
+// }   
+
+
 #include<stdio.h>
 int main() {
-    int true = 1;
-    int false = 0;
-    printf(" ini adalah nilai true dan true : %d \n", true && true);
-    printf(" ini adalah nilai tre dan false : %d \n", true || false);
-    printf(" ini adalah nilai false dan true : %d \n", false && true);
-    printf(" ini adalah nilai false dan false : %d \n", false || false);
-    printf(" ini adalah nilai not true : %d \n", !true);
-    printf(" ini adalah nilai not false : %d \n", !false);
-    printf("-----------\n");
-    printf("OR T F \n");
-    printf("T %d %d \n", true || true, true || false);
-    printf("T %d %d \n",false || true, false || false);
-    printf("-----------\n");
+    int x=5;
+    int y=10;
 
-    printf("-----------\n");
-    printf("AND %d %d \n",true && true,false || false);
-    printf("%d %d %d \n",true || false,true || false,false && false);
-    printf("%d %d %d \n",false || false, false && true,false && true);
-    printf("-----------\n");
+    printf("hasil x<y : %d \n",x<y);
+    printf("hasil x>y : %d \n",x>y);
+    printf("hasil x<=y : %d \n",x<=y);
+    printf("hasil x>=y : %d \n",x>=y);
+    printf("hasil x==y : %d \n",x==y);
+    printf("hasil x!=y : %d \n",x!=y);
 
-}   
-
-
+    printf("hasil x==x : %d \n",x!=y);
+    printf("hasil x!=x : %d \n",x!=x);
+    return 0;
+}
