6926055a9ad48140860377d8cfb53b1262c1617d
fhanus <126766017+fhanus123@users.noreply.github.com>
2024-09-14 04:29:13 +0000

    tambah

--- brlajar/test.c
+++ brlajar/test.c
@@ -2,13 +2,13 @@
 int main() {
     int true = 1;
     int false = 0;
-    int o = "ORTF";
+    int o = ORTF;
     printf(" ini adalah nilai true dan true : %d \n", true && true);
     printf(" ini adalah nilai tre dan false : %d \n", true || false);
     printf(" ini adalah nilai false dan true : %d \n", false && true);
     printf(" ini adalah nilai false dan false : %d \n", false || false);
     printf(" ini adalah nilai not true : %d \n", !true);
     printf(" ini adalah nilai not false : %d \n", !false);
-    printf(" o r t f : \n", o );
+    printf(" o r t f : %c \n", o );
 }
 
