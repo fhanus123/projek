743e735e64f36886aee12ddc83186f6fe1c4687f
fhanus <126766017+fhanus123@users.noreply.github.com>
2024-09-19 04:09:22 +0000

    tabah

--- brlajar/belajar.js
+++ brlajar/belajar.js
@@ -1,11 +1,11 @@
-    // console.log("hello world !")
-    // alert("hati-hati pada orang berbaju hitm ")
-    // document.write("hello world! <br>")
+// console.log("hello world !")
+// alert("hati-hati pada orang berbaju hitm ")
+// document.write("hello world! <br>")
 
 
-    // let arce = 100
+// let arce = 100
 
-    // document.write(arce)
+// document.write(arce)
 
 // function confirme() {
 //     let yakin = confirm("apakah anda yakin untuk menuju ke sana ?")
@@ -30,11 +30,51 @@
 //     } else {
 //         alert("anda tidak memasukan apa apa ")
 //     }
-    
+
 // }
 
-let aku = 20
-let kamu = 20;
+// let pertanyaan = confirm("apakah kamu manusia")
+//         let hasil = pertanyaan ? "selamat datang manusia" : "pergi kamu robot"
+//         alert(hasil)
 
-let hasil = aku == kamu 
-    document.write('${aku}')
+// let uang = prompt("total uang yang anda punya", 0)
+// let barang = prompt("berapa barang yang ingin anda beli", 0)
+// let kembalian = uang - barang
+// if (uang > barang) {
+//     alert("pembelian anda sudah berhasil")
+// } 
+// if (kembalian >= 0) {
+//     alert(`total kembalian :${kembalian}`)
+// }
+// else {
+//     alert("pembelian anda di batalkan")
+// }
+// document.write("terima kasih sudah berbelanja di sini")
+
+let jawab = prompt("selamat kamu sangat beruntung pilihlah kadonya dari no 1 sampe 5")
+let hadiah = ""
+switch (jawab) {
+    case "1":
+        hadiah = "tisu"
+        break
+    case "2":
+        hadiah = "mobil"
+        break
+    case " 3":
+        hadiah = "motor"
+        break
+    case "4":
+        hadiah = "rumah"
+        break
+    case "5":
+        hadiah = "uang Rp50.000"
+        break
+    default:
+        document.write("sepertinya anda slaah memilih")
+}
+
+if (hadiah === "") {
+    alert("kamu gagal mendapatkan hadiah")
+} else {
+    alert("selamat kamu memenangkan hadiah " + hadiah + "")
+}
